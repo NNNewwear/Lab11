23a82c96c4c553ba40019ec6b92afb1cf48aa098
NNNewwear <nextkantapong@gmail.com>
2023-12-22 17:50:39 +0700

    Lab11

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,17 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<string>
+#include<cstdlib>
+#include<time.h>
+using namespace std;
+
+int main()
+{
+    string b[9]={"A","B+","B","C+","C","D+","D","F","W"};
+    cout<<"Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+    srand(time(0));
+    cout<<"You will get "<<b[rand()%9]<<" in this 261102.";
+    return 0;
+}
